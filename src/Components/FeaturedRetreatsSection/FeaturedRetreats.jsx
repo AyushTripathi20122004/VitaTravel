7a31246926c25c3ccb779f30528250c006e8fe8f
Ayush Sharms <ayushsharma48702@gmail.com>
2026-08-06 17:53:02 +0530

    deploy again

--- src/Components/FeaturedRetreatsSection/FeaturedRetreats.jsx
+++ src/Components/FeaturedRetreatsSection/FeaturedRetreats.jsx
@@ -9,9 +9,10 @@ const FeaturedRetreats = () => {
                     <h2 className="text-5xl font-semibold m-0">Featured Retreats</h2>
                 </div>
                 <div className="featured-retreats-small-text  lg:w-[55%]">
-                    <p className="m-0 lg:text-xs lg:w-xs text-lg text-white/50">
+                    <p className="m-0 lg:text-xs lg:w-xs text-lg  text-white/50">
                         Discover unique locations, engaging activities, and expert- 
-                        led workshops designed to inspire and refresh your spirit.</p>
+                        led workshops designed to inspire and refresh your spirit.
+                    </p>
                 </div>
             </div>
             <RetreatsCards />
